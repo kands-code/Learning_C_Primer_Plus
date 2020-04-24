3e22f0a9ae7e2ceefd8d87ab5a5a5d523cebde16
kands-code <59006329+kands-code@users.noreply.github.com>
2020-04-25 02:24:54 +0800

    Update pe06.c
    hello

--- chapter10/pe06.c
+++ chapter10/pe06.c
@@ -1,1 +1,40 @@
-/* pe06.c --  */
+/* pe06.c -- reverses the array */
+#include <stdio.h>
+#define LEN 5
+
+void rev(double * arr, int len);
+
+int main(int argc, char const *argv[])
+{
+	int i;
+	double arr[LEN] = {1.1, 1.2, 1.3, 1.4, 1.5};
+
+	for (i = 0; i < LEN; ++i)
+	{
+		printf("%f\t", arr[i]);
+	}
+	putchar('\n');
+
+	rev(arr, LEN);
+
+	for (i = 0; i < LEN; ++i)
+	{
+		printf("%f\t", arr[i]);
+	}
+	putchar('\n');
+
+	return 0;
+}
+
+void rev(double * arr, int len)
+{
+	int i;
+	double temp;
+
+	for (i = 0; i < (len - 1) / 2; ++i)
+	{
+		temp = arr[i];
+		arr[i] = arr[len - 1 - i];
+		arr[len - 1 - i] = temp;
+	}
+}
